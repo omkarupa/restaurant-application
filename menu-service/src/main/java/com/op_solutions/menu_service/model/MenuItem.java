25ba453c6536414615c51e026881d413e86bfc2a
Omkar Upadhyay <omkarmh009@gmail.com>
2026-05-21 22:47:25 +0530

    added Indexing for MenuItem for column name inside @Table(indexes = {@index(name="index_name" , columnList ="columnname"}

--- menu-service/src/main/java/com/op_solutions/menu_service/model/MenuItem.java
+++ menu-service/src/main/java/com/op_solutions/menu_service/model/MenuItem.java
@@ -8,12 +8,15 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Table(indexes = { @Index(name = "idx_menu_item_name", columnList = "name") })
 @Getter
 @Setter
 public class MenuItem {
