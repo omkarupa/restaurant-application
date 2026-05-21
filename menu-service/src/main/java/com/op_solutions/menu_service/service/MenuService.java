ec0c1a8e683cef59e7e4226a0e5aa69dddd84dd3
Omkar Upadhyay <omkarmh009@gmail.com>
2026-05-21 22:58:55 +0530

    create service method for fetching searchname

--- menu-service/src/main/java/com/op_solutions/menu_service/service/MenuService.java
+++ menu-service/src/main/java/com/op_solutions/menu_service/service/MenuService.java
@@ -2,9 +2,13 @@ package com.op_solutions.menu_service.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.op_solutions.menu_service.model.MenuItem;
+import com.op_solutions.menu_service.repository.MenuRepo;
 
 import lombok.RequiredArgsConstructor;
 
@@ -33,6 +37,25 @@ public class MenuService {
 		
 		repo.saveAll(items);
 		
+	}
+
+	public MenuItem getMenuItem(Long menuItemId) {
+		
+		return repo.findById(menuItemId).orElseThrow(() -> new RuntimeException("Menu item not found with id : " + menuItemId));
+	}
+
+	public Page<MenuItem> getMenuItemByName(String name, int page, int size) {
+		
+		Pageable pageable = PageRequest.of(page, size); // example: first page with 10 items
+		
+		 long start = System.currentTimeMillis();
+		Page<MenuItem> menuItems =  repo.findItemsByName(name,pageable);
+		
+		 long end = System.currentTimeMillis();
+		 long duration = end - start;
+		 System.out.println("Time taken to fetch menu items by name: " + duration + " ms");
+		
+		return menuItems;
 	}
 	
 }
