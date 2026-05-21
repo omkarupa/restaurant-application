d16af336f17df90536a4d916ccb1163976048335
Omkar Upadhyay <omkarmh009@gmail.com>
2026-05-21 22:46:02 +0530

    added get menu by item name for checking performance and used return type of Page and we need to pass page,size as RequestParam

--- menu-service/src/main/java/com/op_solutions/menu_service/controller/MenuItemRestController.java
+++ menu-service/src/main/java/com/op_solutions/menu_service/controller/MenuItemRestController.java
@@ -2,10 +2,12 @@ package com.op_solutions.menu_service.controller;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.op_solutions.menu_service.model.MenuItem;
@@ -15,15 +17,28 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/menuItem")
+@RequestMapping("/api/menuItems")
 public class MenuItemRestController {
 	
 	private final MenuService service;
+
 	
-	@GetMapping("/{restaurantId}")
+	@GetMapping("/restaurant/{restaurantId}")
 	public ResponseEntity<List<MenuItem>> getMenuList(@PathVariable Long restaurantId)
 	{
 		return ResponseEntity.ok(service.getMenuList(restaurantId));
 	}
+	@GetMapping("/{menuItemId}")
+	public ResponseEntity<MenuItem> getMenuItem(@PathVariable Long menuItemId)
+	{
+		return ResponseEntity.ok(service.getMenuItem(menuItemId));
+	}
+	
+	@GetMapping("/itemName/{name}")
+	public ResponseEntity<Page<MenuItem>> getMenuItemByName(@PathVariable String name ,@RequestParam(defaultValue = "0") int page  ,@RequestParam(defaultValue = "10") int size)
+	{
+		return ResponseEntity.ok(service.getMenuItemByName(name,page,size));
+	}
+	
 
 }
